8922778e1e8788901d5893e30978aec90251a28e
VladiqDanilov <90576351+VladiqDanilov@users.noreply.github.com>
2023-11-23 17:48:18 +0300

    Update test_ops.py
    take_1

--- test_ops.py
+++ test_ops.py
@@ -1,13 +1,25 @@
 from ops import *
 
 def test_add():
-    assert add(2,3) == 5
+    assert add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(2, 3) == -1
+    assert subtract(5, 3) == 2
 
 def test_multiply():
     assert multiply(2, 3) == 6
 
 def test_divide():
-    assert divide(10,5) == 2
+    assert divide(10, 5) == 1
+
+def test_power():
+    assert power(2, 3) == 8
+
+def test_square():
+    assert square(4) == 16
+
+def test_cube():
+    assert cube(3) == 27
+
+def test_modulo():
+    assert modulo(10, 3) == 1
